995a9f0121d2db2152a06847ec9e2996707c06e2
Marcela Brenes Daniels <marce89455@gmail.com>
2021-01-23 20:44:23 -0600

    Versión 2

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+
+	$("h3,h4").dblclick(function () {
+	  $(this).css({
+		"color": "red",
+		"font-size": "2em"
+	  })
+	})
+	
+  })
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+//$(this).css("color",'red');
+//$('#tituloingrediente').on('bblclick','h4',function () {
+	//$(this).css("color",'red');
+//});
+
+    
+    
+    
+  
